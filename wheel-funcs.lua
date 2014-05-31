0105dc8744f0bf9a375734e8f29ac6fa8b6964f8
Christian D. Madsen <sevenplagues@gmail.com>
2014-05-30 20:57:12 -0600

    devcode backup
    fixed some wheel things, but I'm thinking of using a different process,
    so I'm saving a potential rollback point here...

--- wheel-funcs.lua
+++ wheel-funcs.lua
@@ -5,15 +5,22 @@ return {
 	generateCombinatorics = function()
 
 		data.scales = buildScales()
-		data.scales = purgeIdenticalScales(data.scales)
+		--data.scales = purgeIdenticalScales(data.scales)
+
 		data.scales = addIntervalSpectrum(data.scales)
-		data.scales = addConsonanceRatings(data.scales)
 
 		data.scales = indexByNoteQuantity(data.scales)
 
-		for k, v in pairs(data.scales[7]) do -- DEBUGGING
+		--data.scales = addIdealRatings(data.scales)
+
+		data.scales = addConsonanceRatings(data.scales)
+
+		table.sort(data.scales[5].scales, function(a,b) return a.conso < b.conso end) -- DEBUGGING
+		for k, v in ipairs(data.scales[5].scales) do -- DEBUGGING
 			print("SCALE " .. v.bin .. ": ")
-			print(v.conso)
+			print("most consonant: " .. data.scales[5].con)
+			print("most dissonant: " .. data.scales[5].dis)
+			print("consonance: " .. v.conso)
 			print(" ")
 		end -- DEBUGGING
 
@@ -22,35 +29,56 @@ return {
 	end,
 
 	-- Assign a consonance rating to each given scale
-	addConsonanceRatings = function(scales)
+	addConsonanceRatings = function(t)
 
-		for k, v in pairs(scales) do
+		for k, v in pairs(t) do
 
-			local avg, avgdiv = 0, 0
-			local compare = deepCopy(v.ints)
+			t[k].avg = 0
+			t[k].con = 12
+			t[k].dis = 0
+			local kdiv = 0
 
-			-- Get the ideal consonance value
-			local ideal = 12 / v.ints[1]
+			for sk, s in pairs(v.scales) do
 
-			-- Collapse the latter half of the intervals into the equivalent former half
-			for i = 8, 12 do
-				local adj = (12 - i) + 1
-				compare[adj] = compare[adj] + compare[i]
+				local avg, avgdiv = 0, 0
+				local compare = deepCopy(s.ints)
+
+				-- Collapse the latter half of the intervals into the equivalent former half
+				for i = 8, 12 do
+					local adj = (12 - i) + 1
+					compare[adj] = compare[adj] + compare[i]
+				end
+
+				-- Increase the average, and average-divisor, by integer-spectrum values
+				for i = 2, 7 do
+					avgdiv = avgdiv + compare[i]
+					avg = avg + (i * compare[i])
+				end
+
+				-- Add this scale's averaged integer-spectrum values to the global total
+				kdiv = kdiv + avgdiv
+				t[k].avg = t[k].avg + avg
+
+				-- Get the scale's consonance-value
+				t[k].scales[sk].conso = avg / avgdiv
+
 			end
 
-			-- Increase the average, and average-divisor, by integer-spectrum values
-			for i = 1, 6 do
-				avgdiv = avgdiv + compare[i]
-				avg = avg + (i * compare[i])
-			end
+			-- Get the k-species' average consonance value,
+			-- which is the most-consonant possible value for the k-species.
+			t[k].avg = t[k].avg / kdiv
 
-			-- Set the scale's consonance-value to the difference between
-			-- the scale's average consonance and the ideal consonance-value.
-			scales[k].conso = math.abs(ideal - (avg / avgdiv))
+			-- Adjust each scale's consonance-value based on the ideal,
+			-- and find the k-species' most consonant and dissonant limits.
+			for sk, s in pairs(v.scales) do
+				t[k].scales[sk].conso = math.abs(s.conso - t[k].avg)
+				t[k].con = math.min(t[k].con, t[k].scales[sk].conso)
+				t[k].dis = math.max(t[k].dis, t[k].scales[sk].conso)
+			end
 
 		end
 
-		return scales
+		return t
 
 	end,
 
@@ -129,16 +157,16 @@ return {
 	end,
 
 	-- Build all fully cyclic combinatoric wheels
-	buildWheels = function(scales)
+	buildWheels = function(t)
 
 		local wheels = {}
 
 		-- For every scale (indexed by numer of notes)...
-		for k, v in pairs(scales) do
+		for k, v in pairs(t) do
 
 			wheels[k] = {}
 
-			for sk, s in pairs(v) do
+			for sk, s in pairs(v.scales) do
 
 				local links, positions = {}, {}
 
@@ -163,15 +191,15 @@ return {
 	end,
 
 	-- Index a given table of scales by how many notes they contain
-	indexByNoteQuantity = function(scales)
+	indexByNoteQuantity = function(t)
 
 		local out = {}
 		for i = 0, 12 do
-			out[i] = {}
+			out[i] = {scales = {}}
 		end
 
-		for k, v in pairs(scales) do
-			table.insert(out[v.ints[1]], v)
+		for k, v in pairs(t) do
+			table.insert(out[v.ints[1]].scales, v)
 		end
 
 		return out
