37905d154fc4df81d46c00809ab323c5750e12be
Christian D. Madsen <sevenplagues@gmail.com>
2014-07-08 09:02:37 -0600

    refactoring Generator Mode things
    yes

--- funcs/generator-funcs.lua
+++ funcs/generator-funcs.lua
@@ -1,6 +1,80 @@
 
 return {
 
+	getGrainFactors = function(beat, grain, fill, putzero, increase)
+
+		local factors = getFactors(beat)
+
+		-- Get all beat-factors equal to or larger than the beatgrain
+		for i = #factors, 1, -1 do
+			if factors[i] < grain then
+				table.remove(factors, i)
+			end
+		end
+
+		-- If no beat-factors were smaller than beatgrain, use beatlength instead
+		if #factors == 0 then
+
+			table.insert(factors, beat)
+
+		-- Else, if there's more than one beatfactor...
+		elseif #factors > 1 then
+
+			-- Double every factor, until surpassing the beatbound limit
+			local newfactors = {}
+			local larger = false
+			local i = 2
+			while not larger do
+				for k, v in pairs(factors) do
+					local nf = v * i
+					if nf < fill then
+						table.insert(newfactors, nf)
+					else
+						larger = true
+					end
+				end
+				i = i + 1
+			end
+
+			-- Combine the new-factors and old-factors, remove duplicates, and sort
+			factors = tableCombine(factors, newfactors)
+			removeDuplicates(factors)
+			table.sort(factors)
+
+			-- Generate sub-factors
+			for i = #factors, 2, -1 do
+				for i2 = i - 1, 1, -1 do
+					local twofactors = factors[i] + factors[i2]
+					if twofactors < fill then
+						table.insert(factors, twofactors)
+					end
+				end
+			end
+
+		end
+
+		-- Insert a 0 into the factors table, if putzero-flag is true
+		if putzero then
+			table.insert(factors, 1, 0)
+		end
+
+		-- Remove duplicate sub-factors, and sort again
+		removeDuplicates(factors)
+		table.sort(factors)
+
+		print(table.concat(factors, ", ")) -- debugging
+		
+		-- Make beat-factors correspond to the 1-indexing, if increase-flag is true
+		if increase then
+			for i = 1, #factors do
+				factors[i] = factors[i] + 1
+			end
+		end
+
+		return factors
+
+	end,
+
 	-- Generate a sequence of notes, following user-defined parameters
 	generateSeqNotes = function(seq, dist, undo)
 
@@ -110,78 +184,21 @@ return {
 
 		end
 
-		local beatfactors = getFactors(data.beatlength)
-		local notefactors = deepCopy(beatfactors)
+		local beatfactors = getGrainFactors(
+			data.beatlength, -- Factor source: non-TPQ secondary beat-length
+			data.beatgrain, -- Minimal acceptable factor size
+			data.beatbound * data.tpq * 4, -- Doubling limit: beatmod times beats
+			true, -- Insert a 0 into the factor list's first position
+			true -- Increase each factor by 1
+		)
 
-		-- Get all beat-factors equal to or larger than the beatgrain
-		for i = #beatfactors, 1, -1 do
-			if beatfactors[i] < data.beatgrain then
-				table.remove(beatfactors, i)
-			end
-		end
-
-		-- If no beat-factors were smaller than beatgrain, use beatlength instead
-		if #beatfactors == 0 then
-
-			table.insert(beatfactors, data.beatlength)
-
-		-- Else, if there's more than one beatfactor...
-		elseif #beatfactors > 1 then
-
-			-- Double every factor, until surpassing the beatbound limit
-			local newfactors = {}
-			local larger = false
-			local i = 2
-			while not larger do
-				for k, v in pairs(beatfactors) do
-					local nf = v * i
-					if nf < (data.beatbound * data.tpq * 4) then
-						table.insert(newfactors, nf)
-					else
-						larger = true
-					end
-				end
-				i = i + 1
-			end
-
-			-- Combine the new-factors and old-factors, remove duplicates, and sort
-			beatfactors = tableCombine(beatfactors, newfactors)
-			removeDuplicates(beatfactors)
-			table.sort(beatfactors)
-
-			-- Generate sub-factors
-			for i = #beatfactors, 2, -1 do
-				for i2 = i - 1, 1, -1 do
-					local twofactors = beatfactors[i] + beatfactors[i2]
-					if twofactors < (data.beatbound * data.tpq * 4) then
-						table.insert(beatfactors, twofactors)
-					end
-				end
-			end
-
-			-- Remove duplicate sub-factors, insert the base tick, and sort again
-			removeDuplicates(beatfactors)
-			table.insert(beatfactors, 1, 0)
-			table.sort(beatfactors)
-
-			print(table.concat(beatfactors, ", ")) -- debugging
-			
-			-- Make the beat-factors correspond to the 1-indexing of ticks
-			for i = 1, #beatfactors do
-				beatfactors[i] = beatfactors[i] + 1
-			end
-
-		end
-
-		-- Generate all acceptable note-sizes
-		for i = #notefactors, 1, -1 do
-			if notefactors[i] < data.notegrain then
-				table.remove(notefactors, i)
-			end
-		end
-		if #notefactors == 0 then
-			table.insert(notefactors, data.notegrain)
-		end
+		local notefactors = getGrainFactors(
+			data.beatlength,
+			data.notegrain,
+			data.beatlength,
+			false,
+			false
+		)
 
 		-- Select the ticks that will be used to allocate notes,
 		-- based on random chance, until the density quotient is fulfilled.
